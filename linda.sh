3ce656cccd5db87709da32f0747a727e93b222fb
linda <lindaochem@gmail.com>
2024-03-04 10:02:10 +0100

    init

--- linda.sh
+++ linda.sh
@@ -6,6 +6,15 @@ read -p "Enter commit message: " commit_message
 # Interactive prompt for branch name
 read -p "Enter branch name: " branch_name
 
+# Check if the branch already exists
+if git rev-parse --verify "$branch_name" >/dev/null 2>&1; then
+  echo "Branch '$branch_name' already exists."
+else
+  # Create a new branch
+  git checkout -b "$branch_name"
+  echo "New branch '$branch_name' created."
+fi
+
 # Add all changes
 git add .
 
@@ -16,32 +25,3 @@ git commit -m "$commit_message"
 git push origin "$branch_name"
 
 echo "Code successfully pushed to branch $branch_name with commit message: $commit_message"
-
-
-
-
-
-# #!/bin/bash
-
-# # Check if user provided commit message and branch name
-# if [ $# -lt 2 ]; then
-#     echo "Usage: $0 <commit_message> <branch_name>"
-#     exit 1
-# fi
-
-# # Commit message
-# commit_message="$1"
-
-# # Branch name
-# branch_name="$2"
-
-# # Add all changes
-# git add .
-
-# # Commit changes with provided message
-# git commit -m "$commit_message"
-
-# # Push changes to specified branch
-# git push origin "$branch_name"
-
-# echo "Code successfully pushed to branch $branch_name with commit message: $commit_message"
